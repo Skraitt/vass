dd07ee1bd504c8cec857f277b486596a7a9b31e2
Javier <nejhernandezb@correo.udistrital.edu.co>
2021-09-30 08:04:04 -0500

    Se corrigio la vista en diferentes resoluciones

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,49 +11,51 @@ export default function Footer() {
   return (
     <div className="footer container-fluid bg-lightdark p-0">
       <section className="container-xxl py-5">
-        <div className="footer__container row-cols-4 d-flex p-3 ">
+        <div className="footer__container row-cols-2 d-flex p-3 ">
           <section className="footer__container__logo p-3 col-sm-1 d-flex justify-content-between  flex-column">
             <h5 className="text-right font-weight-bold text-white footer__title">Complex made simple</h5>
             <img className="footer__container__logo--logo align-self-end d-none d-md-block " src={logo} alt="Vass" />
           </section>
-          <section className="footer__container__links col-sm-5 d-flex  flex-column footer__section">
-            <h6 className="font-weight-bold d-flex text-white">Sitemap VASS</h6>
-            <a className="footer__links" href="">Home</a>
-            <a className="footer__links" href="">Casos éxito</a>
-            <a className="footer__links" href="">Somos VASS</a>
-            <a className="footer__links" href="">Impactos</a>
-            <a className="footer__links" href="">Método</a>
-            <a className="footer__links" href="">Tecnologías</a>
-            <a className="footer__links" href="">Proyectos I+D+i</a>
-            <a className="footer__links" href="">Insights</a>
-            <a className="footer__links" href="">Noticias</a>
-            <a className="footer__links" href="">VASS Research</a>
-            <a className="footer__links" href="">Canal de denuncias</a>
-            <a className="footer__links" href="">Contacto</a>
-          </section>
-          <section className="footer__container__links col-sm-4 d-flex flex-column">
-            <h6 className="font-weight-bold text-white">Sitemap VASS</h6>
-            <a className="footer__links" href="">Talento</a>
-            <a className="footer__links" href="">#LifeVASS</a>
-            <a className="footer__links" href="">Beneficios</a>
-            <a className="footer__links" href="">Planes para ti</a>
-            <a className="footer__links" href="">Proyectos</a>
-            <a className="footer__links" href="">Smartworking</a>
-            <a className="footer__links" href="">Ofertas</a>
-          </section>
-          <section className="col-md-2 footer__container__links  d-flex flex-column justify-content-between">
-            <div>
-              <h6 className="font-weight-bold text-white">Empresas</h6>
-              <a className="footer__links" href="">VASS</a>
-              <a className="footer__links" href="">Nateevo</a>
-              <a className="footer__links" href="">Serbatic</a>
-              <a className="footer__links" href="">vdSHOP</a>
+          <section className="footer__container__cols container-fluid d-flex  m-0 col-md-11 row row-cols-auto">
+            <div className="footer__container__links col-md-5 d-flex  flex-column footer__section pl-3">
+              <h6 className="font-weight-bold d-flex text-white">Sitemap VASS</h6>
+              <a className="footer__links" href="">Home</a>
+              <a className="footer__links" href="">Casos éxito</a>
+              <a className="footer__links" href="">Somos VASS</a>
+              <a className="footer__links" href="">Impactos</a>
+              <a className="footer__links" href="">Método</a>
+              <a className="footer__links" href="">Tecnologías</a>
+              <a className="footer__links" href="">Proyectos I+D+i</a>
+              <a className="footer__links" href="">Insights</a>
+              <a className="footer__links" href="">Noticias</a>
+              <a className="footer__links" href="">VASS Research</a>
+              <a className="footer__links" href="">Canal de denuncias</a>
+              <a className="footer__links" href="">Contacto</a>
             </div>
-            <div className="d-none d-md-block ">
-              <img className="companies__icon m-1" src={icon1} alt="" />
-              <img className="companies__icon m-1" src={icon2} alt="" />
-              <img className="companies__icon m-1" src={icon3} alt="" />
-              <img className="companies__icon m-1" src={icon4} alt="" />
+            <div className="footer__container__links col-md-4 d-flex flex-column pl-5">
+              <h6 className="font-weight-bold text-white">Sitemap VASS</h6>
+              <a className="footer__links" href="">Talento</a>
+              <a className="footer__links" href="">#LifeVASS</a>
+              <a className="footer__links" href="">Beneficios</a>
+              <a className="footer__links" href="">Planes para ti</a>
+              <a className="footer__links" href="">Proyectos</a>
+              <a className="footer__links" href="">Smartworking</a>
+              <a className="footer__links" href="">Ofertas</a>
+            </div>
+            <div className="col-md-3 footer__container__links  d-flex flex-column justify-content-between align-items-end">
+              <div>
+                <h6 className="font-weight-bold text-white">Empresas</h6>
+                <a className="footer__links" href="">VASS</a>
+                <a className="footer__links" href="">Nateevo</a>
+                <a className="footer__links" href="">Serbatic</a>
+                <a className="footer__links" href="">vdSHOP</a>
+              </div>
+              <div className="d-none d-md-block ">
+                <img className="companies__icon m-1" src={icon1} alt="" />
+                <img className="companies__icon m-1" src={icon2} alt="" />
+                <img className="companies__icon m-1" src={icon3} alt="" />
+                <img className="companies__icon m-1" src={icon4} alt="" />
+              </div>
             </div>
           </section>
         </div>
